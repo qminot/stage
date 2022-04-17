738d3e7e3907d762f4cb8b7bbfead1f364c1db56
qminot <qminot75013@gmail.com>
2022-04-17 18:57:06 +0200

    ajout de la connexion

--- site/controleur/monProfil.php
+++ site/controleur/monProfil.php
@@ -2,7 +2,7 @@
 if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
-include_once "$racine/modele/authentification.inc.php";
+include_once "$racine/modele/authentification.inc.php"; 
 include_once "$racine/modele/bd.utilisateur.inc.php";
 include_once "$racine/modele/bd.equipe.inc.php";
 include_once "$racine/modele/bd.joueur.inc.php";
